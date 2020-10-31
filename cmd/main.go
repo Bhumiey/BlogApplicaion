319c5342e4736c8f20d78d7ba30364d4b9000664
Bhumi Patel <patelbhumi.1010@gmail.com>
2020-10-31 20:15:38 +0530

    Change

--- cmd/main.go
+++ cmd/main.go
@@ -8,10 +8,5 @@ import (
 func main() {
 	fmt.Println("Hello")
 	Router.StartServer()
-	// r := chi.NewRouter()
-	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	json.NewEncoder(w).Encode("Hey you all i got first result")
-	// 	// w.Write([]byte("Welcome"))
-	// })
-	// http.ListenAndServe(":8080", r)
+
 }
